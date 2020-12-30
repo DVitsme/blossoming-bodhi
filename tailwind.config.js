7aefbdd7356e7bf2e4eb6cc4e7163d5b3340a6f0
DVitsme <derrick.valentine@gmail.com>
2020-12-30 02:19:41 -0500

    Support for 1/7 in width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 // const forms = require('tailwindcss/forms');
 
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       colors,
+      width: {
+        "1/7": "14.2857143%",
+        "2/7": "28.5714286%",
+        "3/7": "42.8571429%",
+        "4/7": "57.1428571%",
+        "5/7": "71.4285714%",
+        "6/7": "85.7142857%",
+      },
     },
   },
   variants: {},
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require("@tailwindcss/forms")],
   // xwind options
   xwind: {
-    mode: 'objectstyles',
+    mode: "objectstyles",
   },
 };
