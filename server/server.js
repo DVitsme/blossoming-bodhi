361f494f3dfac0f230ccce5bec3f1916120e7da5
DVitsme <derrick.valentine@gmail.com>
2021-09-02 10:44:38 -0400

    server init w/ mongo

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
+import mongoose from 'mongoose';
 const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch((err) => console.log('DB error:', err));
 
 // middleware
 app.use(cors());
